338b57029b7cee0fb75b349f7bd401b35e4e7e0f
Joshua Peisach <itzswirlz2020@outlook.com>
2023-02-08 09:06:01 -0500

    Update translation strings
    dialogs - be gettext compatible
    update po strings
    remove useless en_US translation, we're writing in US english

--- src/dialogs.rs
+++ src/dialogs.rs
@@ -1,3 +1,4 @@
+use gettextrs::*;
 use gtk4::{prelude::DialogExtManual, traits::GtkWindowExt, *};
 
 fn show_error_dialog(window: &ApplicationWindow, message: &str) {
@@ -16,9 +17,7 @@ fn show_error_dialog(window: &ApplicationWindow, message: &str) {
 pub fn show_load_saved_stocks_failed_dialog(window: &ApplicationWindow) {
     show_error_dialog(
         window,
-        concat!(
-            "The saved stocks could not be loaded. Try restaring the app.\n",
-            "\nTo prevent data loss, saving/unsaving stocks will be disabled until this is fixed."
+        &gettext("The saved stocks could not be loaded. Try restaring the app.\n\nTo prevent data loss, saving and unsaving stocks will be disabled until this is fixed.",
         ),
     )
 }
@@ -26,20 +25,20 @@ pub fn show_load_saved_stocks_failed_dialog(window: &ApplicationWindow) {
 pub fn show_saving_unsaving_disabled_dialog(window: &ApplicationWindow) {
     show_error_dialog(
         window,
-        "Saving/unsaving stocks is disabled to prevent data loss. Try restarting the app.",
+        &gettext("Saving and unsaving stocks is disabled to prevent data loss. Try restarting the app."),
     );
 }
 
 pub fn show_save_stock_failed_dialog(window: &ApplicationWindow) {
     show_error_dialog(
         window,
-        "An error occurred and the stock could not be saved. Try saving it again.",
+        &gettext("An error occurred and the stock could not be saved. Try saving it again."),
     )
 }
 
 pub fn show_unsave_stock_failed_dialog(window: &ApplicationWindow) {
     show_error_dialog(
         window,
-        "An error occured and the stock could not be unsaved. Try unsaving it again.",
+        &gettext("An error occured and the stock could not be unsaved. Try unsaving it again."),
     );
 }
