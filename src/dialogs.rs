46885dc85fc053d51588d4775900db60be9fa2d9
gltchitm <42951031+gltchitm@users.noreply.github.com>
2023-02-14 13:15:40 -0500

    Remediate typographical errors

--- src/dialogs.rs
+++ src/dialogs.rs
@@ -17,7 +17,7 @@ fn show_error_dialog(window: &ApplicationWindow, message: &str) {
 pub fn show_load_saved_stocks_failed_dialog(window: &ApplicationWindow) {
     show_error_dialog(
         window,
-        &gettext("The saved stocks could not be loaded. Try restaring the app.\n\nTo prevent data loss, saving and unsaving stocks will be disabled until this is fixed.",
+        &gettext("The saved stocks could not be loaded. Try restarting the app.\n\nTo prevent data loss, saving and unsaving stocks will be disabled until this is fixed.",
         ),
     )
 }
@@ -41,6 +41,6 @@ pub fn show_save_stock_failed_dialog(window: &ApplicationWindow) {
 pub fn show_unsave_stock_failed_dialog(window: &ApplicationWindow) {
     show_error_dialog(
         window,
-        &gettext("An error occured and the stock could not be unsaved. Try unsaving it again."),
+        &gettext("An error occurred and the stock could not be unsaved. Try unsaving it again."),
     );
 }
